d87edb16e88aa7520b3c9391385fde24b56baaa8
Lijo Skariah <skariahlijo@yahoo.com>
2017-01-24 16:22:43 -0500

    Create First.java

---
+++ First.java
@@ -0,0 +1,13 @@
+
+public class Hello_world {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+
+	}
+
+}
